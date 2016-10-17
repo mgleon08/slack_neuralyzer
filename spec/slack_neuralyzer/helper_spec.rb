38cf9f611340dcb9da66a00410f33395d22ee04e
Leon Ji <mgleon08@gmail.com>
2016-10-17 23:38:43 +0800

    Alter helper spec to pass travis CI

--- spec/slack_neuralyzer/helper_spec.rb
+++ spec/slack_neuralyzer/helper_spec.rb
@@ -5,13 +5,13 @@ describe SlackNeuralyzer::Helper do
 
   it '#parse_to_ts' do
     parse_to_ts('20160101', '20161212')
-    expect(start_time).to eq(1_451_577_600.0)
-    expect(end_time).to eq(1_481_472_000.0)
+    expect(start_time).to eq(Time.parse('20160101').to_f)
+    expect(end_time).to eq(Time.parse('20161212').to_f)
   end
 
   it '#parse_to_date' do
     time = parse_to_date(1_451_577_600.0)
-    expect(time).to eq('2016-01-01 00:00')
+    expect(time).to eq(Time.at('1_451_577_600'.to_f).strftime('%Y-%m-%d %H:%M'))
   end
 
   it '#reset_counter' do
